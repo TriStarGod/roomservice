a3ecb8948a23e03f8a44f3a53517bed66ea75ad7
TriStarGod <dfa2222@gmail.com>
2019-11-16 14:48:41 -0500

    Updates

---
+++ precache-manifest.acdd91d37a2b9daaa67be2988820c731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447c31be1ee34ca4f47aabae5d792f5c",
+    "url": "/TriStarGod/roomservice/index.html"
+  },
+  {
+    "revision": "da21b139fb75ec365614",
+    "url": "/TriStarGod/roomservice/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9fdcba1dd88da08477db",
+    "url": "/TriStarGod/roomservice/static/js/2.655fe3dd.chunk.js"
+  },
+  {
+    "revision": "da21b139fb75ec365614",
+    "url": "/TriStarGod/roomservice/static/js/main.7f5fdf64.chunk.js"
+  },
+  {
+    "revision": "55b5063f069ee8a382e6",
+    "url": "/TriStarGod/roomservice/static/js/runtime-main.adcae071.js"
+  }
+]);
